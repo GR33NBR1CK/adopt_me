5d266de2bced40d192f69f89be93e5fb5f9ecbe5
Filip Surowka <surowkafilip@gmail.com>
2023-03-12 13:23:25 +0100

    create AdoptPetContext, add adopted pet image to form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
 import { useQuery } from "@tanstack/react-query";
 import fetchSearch from "./fetchSearch";
+import AdoptPetContext from "./AdoptPetContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -15,6 +16,7 @@ const searchParams = () => {
     const [animal, setAnimal] = useState("");
     const [breeds] = useBreedList(animal);
     const results = useQuery(["search", requestParams], fetchSearch);
+    const [adoptedPet] = useContext(AdoptPetContext);
 
     const pets = results?.data?.pets ?? [];
     return (
@@ -31,6 +33,14 @@ const searchParams = () => {
                     setRequestParams(data);
                 }}
             >
+                {adoptedPet ? (
+                    <div className="pet image-container">
+                        <img
+                            src={adoptedPet?.images[0]}
+                            alt={adoptedPet?.name}
+                        />
+                    </div>
+                ) : null}
                 <label htmlFor="location">Location</label>
                 <input
                     name="location"
