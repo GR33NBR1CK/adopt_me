11c34925162a31b2b19202f923e135abd2df0add
Filip Surowka <surowkafilip@gmail.com>
2023-10-16 02:22:12 +0200

    add config for test env and using vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     root: "src",
+    test: {
+        environment: "happy-dom",
+        setupFiles: [
+            "./setupVitest.js"
+        ]
+    }
 });
